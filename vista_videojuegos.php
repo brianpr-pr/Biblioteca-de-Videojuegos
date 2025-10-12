4ae66ec4d0d01fef4c9afba888d27e71f2376a94
brianpr-pr <brian23perezperez.com>
2025-10-12 11:56:21 +0100

    Más cambios

--- vista_videojuegos.php
+++ vista_videojuegos.php
@@ -4,5 +4,18 @@ error_reporting(E_ALL & ~E_WARNING);
 ini_set('display_errors', 1);
 $nombreArchivo = basename(path: __FILE__);
 include "./caracteristicas/utilidades/header.php";
-include "./caracteristicas/servidor/datos_servidor.php";
+include "./caracteristicas/servidor/mostrarVideojuegos.php/";
+?>
+
+<h2>Colección de videojuegos</h2>
+<table>
+    <tr>
+        <th>Caratula</th>
+        <th>Título</th>
+        <th>Fecha de lanzamiento</th>
+    </tr>
+    <?php echo mostrarVideojuegos();?>
+</table>
+
+<?
 include "./caracteristicas/utilidades/footer.php";
