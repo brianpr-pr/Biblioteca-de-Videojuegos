6fb1b36a551ce2178416bdf413effac3e3fc12ed
brianpr-pr <brian23perezperez@gmail.com>
2025-10-20 15:23:45 +0100

    Ya se actualizan las fotos de perfil

--- caracteristicas/usuario/usuario.php
+++ caracteristicas/usuario/usuario.php
@@ -4,3 +4,28 @@ function salirUsuario(){
     $_SESSION['email_usuario'] = null;
     header("");
 }
+
+function mostrarImagenPerfil(){
+    try {
+        include "./caracteristicas/servidor/datos_servidor.php";
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", 
+        $username, 
+        $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $conn->prepare("SELECT imagen_perfil from usuarios 
+        WHERE nombre_usuario=:nombre_usuario");
+        $stmt->execute(['nombre_usuario' => $_SESSION['nombre_usuario']]);
+
+        if($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $conn = null;
+            return $row['imagen_perfil'];
+        }
+        $conn = null;
+        return false;
+
+    } catch(PDOException $e) {
+        echo "{$e->getMessage()}<br>{$errorMensaje}";
+    }
+}
--- caracteristicas/utilidades/header.php
+++ caracteristicas/utilidades/header.php
@@ -1,5 +1,7 @@
 <?php
 include "./caracteristicas/utilidades/hojas_estilo_vinculador.php";
+include "./caracteristicas/usuario/usuario.php";
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -17,6 +19,6 @@ include "./caracteristicas/utilidades/hojas_estilo_vinculador.php";
         <a href="inicio_sesion.php">Inicio de sesión</a>
         <a href="vista_videojuegos.php">Biblioteca de videojuegos</a>
         <a href="añadir_videojuego.php">Añadir videojuego</a>
-        <a href="iniciado.php"><img id="imagen-perfil" src="./perfil/default.png"></a>
+        <a href="iniciado.php"><img id="imagen-perfil" src="<?php echo mostrarImagenPerfil();?>"></a>
     </header>
-<?php include "./caracteristicas/servidor/conectar.php";?>
+<?php include "./caracteristicas/servidor/conectar.php"; ?>
